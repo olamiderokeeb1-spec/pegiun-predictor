63a94541673af285069d920472cadfa15a470409
olamiderokeeb1-spec <olamiderokeeb1@gmail.com>
2026-05-10 10:59:41 +0100

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.title("🐧 Penguin Predictor")
+
+species = st.selectbox("Species", ["Adelie", "Chinstrap", "Gentoo"])
+
+island = st.selectbox("Island", ["Biscoe", "Dream", "Torgersen"])
+
+sex = st.radio("Sex", ["Male", "Female"], horizontal=True)
+
+bill_length_mm = st.number_input(
+    "Bill length (mm)", 30.0, 60.0, 44.0
+)
+
+bill_depth_mm = st.number_input(
+    "Bill depth (mm)", 13.0, 22.0, 17.0
+)
+
+flipper_length_mm = st.number_input(
+    "Flipper length (mm)", 170.0, 235.0, 200.0
+)
+
+st.write("Prediction goes here")
+streamlit==1.39.0
+pandas==2.2.3
+scikit-learn==1.5.2
+joblib==1.4.2
+numpy==2.1.
